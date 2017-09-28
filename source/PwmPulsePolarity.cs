ef4132073d459d7fbc935d35f7005893719bdf7c
José Simões <josesimoes@users.noreply.github.com>
2017-09-28 11:35:26 +0100

    Merge Windows.Devices.Pwm from WIP folder (#1)

---
+++ source/PwmPulsePolarity.cs
@@ -0,0 +1,22 @@
+﻿//
+// Copyright (c) 2017 The nanoFramework project contributors
+// See LICENSE file in the project root for full license information.
+//
+
+namespace temp.Windows.Devices.Pwm
+{
+    /// <summary>
+    /// Describes which polarity the PWM signal should start in.
+    /// </summary>
+    public enum PwmPulsePolarity
+    {
+        /// <summary>
+        /// Configures the PWM signal to start in the active high state.
+        /// </summary>
+        ActiveHigh,
+        /// <summary>
+        /// Configures the PWM signal to start in the active low state.
+        /// </summary>
+        ActiveLow
+    }
+}
